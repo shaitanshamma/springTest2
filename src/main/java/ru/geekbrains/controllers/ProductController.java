fcc2f4a16e8176d6124281762e5330e564d5621a
shaitanshamma <kurbatov_andr@hotmail.com>
2020-01-22 22:25:59 +0300

    Test 2

--- src/main/java/ru/geekbrains/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/controllers/ProductController.java
@@ -4,12 +4,15 @@ package ru.geekbrains.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.entities.Product;
 import ru.geekbrains.services.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -28,6 +31,7 @@ public class ProductController {
         System.out.println(productList);
         return "product-list";
     }
+
     @RequestMapping(value = "/product/add", method = RequestMethod.GET)
     public String addNewProductList(Model model) {
         Product product = new Product();
@@ -36,10 +40,30 @@ public class ProductController {
         model.addAttribute("productAdd", product);
         return "product-add";
     }
+
     @RequestMapping(value = "/product/add", method = RequestMethod.POST)
     public String addNewProductList(Product product) {
         productService.save(product);
         return "redirect:/product/list";
     }
 
+    @RequestMapping(value = "/product/price", method = RequestMethod.GET)
+    public String showProductListInPrice(Model model) {
+        Product product = new Product();
+        model.addAttribute("productPriceList", product);
+        return "product-price-list";
+    }
+//    @RequestMapping(value = "/product/price", method = RequestMethod.POST)
+//    public String showProductListInPrice(Model model, int minPrice, int maxPrice) {
+//        List<Product> productList = productService.myAwerageProduct(minPrice,maxPrice);
+//        model.addAttribute("productPriceList", productList);
+//        return "redirect:/product/list";
+//    }
+
+    @RequestMapping(path = "/showProductById/{min}/{max}", method = RequestMethod.GET)
+    public String showProductById(Model model, @PathVariable(value = "min") int minPrice, @PathVariable(value = "max") int maxPrice) {
+        List<Product> productList = productService.myAwerageProduct(new Integer(minPrice),new Integer(maxPrice));
+        model.addAttribute("product", productList);
+        return "redirect:/product/list";
+    }
 }
